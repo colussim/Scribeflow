0d7510d5ee453cf23d1a71f2af8c0782e183ad01
colussim <mcolussi@bluewin.ch>
2026-09-02 12:03:44 +0200

    rm setconfig.sh

--- setconfig.sh
+++
@@ -1,5 +0,0 @@
-dist/confluence-publish-darwin-arm64 config set \
-  --base-url https://confluence.appls.fr.ch \
-  --token MjgwNTQ4NjMzOTAwOr7UeR74+++3OIkqDspkquDYdjf8 \
-  --space PRJ2043 \
-  --parent 447945799
